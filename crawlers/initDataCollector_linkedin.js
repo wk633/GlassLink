7bc3dd99db516206460a0882f408eae53a5cc45c
wk633 <wk633@outlook.com>
2018-01-04 01:57:40 -0800

    crawl first 200 data from linkedin; must use headless: false, if not will trigger anti-crawler strategy

--- crawlers/initDataCollector_linkedin.js
+++ crawlers/initDataCollector_linkedin.js
@@ -2,12 +2,18 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const utils = require('./utils');
 
+const mongoose = require('mongoose');
+const conn = mongoose.createConnection(require('../config').DB.URL);
+const Linkedin = require('../models/linkedin')(conn);
+
+
+
 const log = console.log
 
 async function main () {
     log('start main function');
     try{
-        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox']});
         const prefix = 'https://www.linkedin.com/jobs/search?';
         let data = {
             'keywords': 'Software+Engineer+Intern',
@@ -16,8 +22,8 @@ async function main () {
             'start': 0,
             'count': 25
         }
-
-        for(let i = 0; i < 1; i++){
+        const page = await browser.newPage();
+        for(let i = 0; i < 8; i++){
             data.start = 25 * i;
             let dataArr = [];
             for(let k in data){
@@ -25,13 +31,10 @@ async function main () {
             }
             const requestUrl = prefix + dataArr.join('&');
             log(requestUrl);
-            const page = await browser.newPage();
-            await page.goto(requestUrl);
+            await page.goto(requestUrl, {waitUntil: 'networkidle2'});
             await page.waitFor(1000); // wait extra 1s
-            log('wait 5s end');
+            // log('wait 5s end');
             const rawJobData = await page.evaluate(()=>{
-                window.scrollTo(0,document.body.scrollHeight);
-
                 const list = document.querySelectorAll('li.job-listing');
                 NodeList.prototype.map = Array.prototype.map;
                 return list.map(item=>{
@@ -50,16 +53,36 @@ async function main () {
                     return tmp;
                 })
             })
-            console.log(rawJobData);
+
             const processed = rawJobData.map(item=>{
                 item.postDate = utils.postDateGen(item.postDateRaw);
                 item.identifier = utils.identifierGen(item.company, item.job, item.location, item.postDate);
                 return item;
             })
-            console.log(processed);
-            await page.close();
-            await browser.close();
+            const length = processed.length;
+            log(`${length} items to be saved`);
+            await Promise.all(processed.map(async(item) => {
+                // let linkedinModel = new Linkedin(item);
+                try{
+                    const d = await Linkedin.findOneAndUpdate({
+                        identifier: item.identifier
+                    },item, {upsert: true, new: true} ) // if not exist, insert it
+                    if(d != null){
+                        log(`${d.company} - ${d.job} saved or updated`)
+                    }else{
+                        log('doc is null')
+                    }
+                    
+                }catch(e){
+                    log('error!!!');
+                    log(e);
+                }
+            }))
+            
         }
+        await page.close();
+        await conn.close();
+        await browser.close();
 
     }catch(e){
         log(e);
