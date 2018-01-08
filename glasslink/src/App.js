8e132d8538644a64adbda6edc8ea7be995ef63a1
wk633 <wk633@outlook.com>
2018-01-08 13:57:26 -0800

    build website frame

--- glasslink/src/App.js
+++ glasslink/src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import {Button} from 'antd';
-import logo from './logo.svg';
+import {Layout, Menu, Breadcrumb} from 'antd';
 import './App.css';
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button type='danger'>button</Button>
-      </div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">Job List</Menu.Item>
+            <Menu.Item key="2">Application Manage</Menu.Item>
+            <Menu.Item key="3">Chat Room</Menu.Item>
+            <Menu.Item key="4">Log In</Menu.Item>
+            <Menu.Item key="5">Sign Up</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>Job List</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+        </Content>
+      </Layout>
     );
   }
 }
