41d363829940fd63980abd107f54c5e6e6e4a7bd
wk633 <wk633@outlook.com>
2018-01-08 13:29:22 -0800

    modify crawler frequency

--- crawlers/monitor.js
+++ crawlers/monitor.js
@@ -2,16 +2,16 @@ var shell = require("shelljs");
 shell.exec('node monitor_linkedin.js');
 setInterval(()=>{
     shell.exec('node monitor_linkedin.js');
-}, 60*1000*10)
+}, 2*60*1000*60)
 
 setTimeout(()=>{
     shell.exec('node monitor_glassdoor.js');
     setInterval(()=>{
         shell.exec('node monitor_glassdoor.js');
-    }, 60*1000*60)
-}, 30*1000*60);
+    }, 2*60*1000*60)
+}, 60*1000*60);
 
 // 00:00  linkedin
-// 00:30  glassdoor
-// 01:00  linkedin
-// 01:30  glassdoor
+// 01:00  glassdoor
+// 02:00  linkedin
+// 03:00  glassdoor
