be19987700d615bb56a92c4991b7afe4d8e75d6c
wk633 <wk633@outlook.com>
2018-01-10 14:24:26 -0800

    add login memory and joblist page framework

--- glasslink/src/component/authrouter/authrouter.js
+++ glasslink/src/component/authrouter/authrouter.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import axios from 'axios';
+import {message} from 'antd';
 import {withRouter} from 'react-router-dom';
+message.config({
+    top: 54
+})
 
 @withRouter
 class AuthRouter extends React.Component{
@@ -8,7 +13,17 @@ class AuthRouter extends React.Component{
         const currentPath = this.props.location.pathname;
         console.log(currentPath);
         if(publicList.indexOf(currentPath) === -1){
-            this.props.history.push('/login');
+            axios.get('/user/info')
+            .then((res) => {
+                if(res.status === 200) {
+                    if(res.data.code === 0){
+                        // have login info
+                    }else{
+                        message.error(res.data.errmsg);
+                        this.props.history.push('/login');
+                    }
+                }
+            })
         }
     }
     render(){
--- glasslink/src/container/joblist/joblist.js
+++ glasslink/src/container/joblist/joblist.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Collapse, Avatar, Badge} from 'antd';
+const Panel = Collapse.Panel;
 
 export default class JobList extends React.Component{
     render(){
         return (
-            <div>JobList</div>
+            <Collapse defaultActiveKey={['1']}>
+                <Panel header="Linkedin" key="1">
+                    <p>linkedin</p>
+                    <p>linkedin</p>
+                </Panel>
+                <Panel header="Glassdoor" key="2">
+                    <p>glassdoor</p>
+                </Panel>
+            </Collapse>
         )
     }
 }
